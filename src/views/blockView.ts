2acf215d86c8c58d5c225eb61b9301410c98d3f2
Alexander van Oostenrijk <alex.vanoostenrijk@gmail.com>
2018-04-23 16:52:03 +0200

    hittest no longer worked on BlockView

--- src/views/blockView.ts
+++ src/views/blockView.ts
@@ -57,6 +57,22 @@ export class BlockView extends BoxView {
     canvas.restore();    
   }
 
+  drawSimple(canvas: IScreen, mouseX: number, mouseY: number, selectionSize: number, hover: boolean) {
+    // Translate to block's coordinates, so we can offset everything from (0,0).
+    canvas
+      .save()
+      .translate(this.block.x, this.block.y);
+
+    this.makeShape(canvas, true);
+
+    // Nearly transparent background (for selection purposes):
+    canvas
+      .fillStyle(Values.COLOR_TRANSPARENT)
+      .fill();
+
+    canvas.restore();        
+  }
+
   drawHandles(canvas: IScreen, mouseX: number, mouseY: number, selectionSize: number, hover: boolean) {  
     this.drawResizeHandles(canvas, mouseX, mouseY, selectionSize, hover);
   }
