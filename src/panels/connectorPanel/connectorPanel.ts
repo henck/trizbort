5e581ff34954423b48a2deb1e2d9cc28ab22635f
Alexander van Oostenrijk <alex.vanoostenrijk@gmail.com>
2018-04-30 09:23:49 +0200

    Connector panel supports line style

--- src/panels/connectorPanel/connectorPanel.ts
+++ src/panels/connectorPanel/connectorPanel.ts
@@ -4,7 +4,7 @@ import { AppEvent } from '../../enums/appEvent.js'
 import { Connector } from '../../models/connector.js';
 import { App } from '../../app.js';
 import { Panel }  from '../panels.js'
-import { IdCheck, IdInput, IdColorPicker } from '../../controls/controls.js';
+import { IdCheck, IdInput, IdColorPicker, IdLineStyle, IdRange } from '../../controls/controls.js';
 import { IdRadio } from '../../controls/idRadio/idRadio.js';
 import { ConnectorType } from '../../enums/connectorType.js';
 import { IdConnectorType } from '../../controls/idConnectorType/idConnectorType.js';
@@ -16,6 +16,8 @@ export class ConnectorPanel extends Panel implements Subscriber {
   private ctrlCurve: IdCheck;
   private ctrlOneWay: IdCheck;
   private ctrlColor: IdColorPicker;
+  private ctrlLineStyle: IdLineStyle;
+  private ctrlLineWidth: IdRange;
   private btnReverse: HTMLLinkElement;
   
   private ctrlStartType: IdConnectorType;
@@ -30,6 +32,8 @@ export class ConnectorPanel extends Panel implements Subscriber {
     this.ctrlName = new IdInput('.js-name', this.elem).addEventListener('input', () => { this.connector.name = this.ctrlName.value; });
     this.ctrlCurve = new IdCheck('.js-curve', this.elem).addEventListener('input', () => { this.connector.isCurve = this.ctrlCurve.checked; });
     this.ctrlOneWay = new IdCheck('.js-oneway', this.elem).addEventListener('input', () => { this.connector.oneWay = this.ctrlOneWay.checked; });
+    this.ctrlLineStyle = new IdLineStyle('.js-linestyle', this.elem).addEventListener('change', () => { this.connector.lineStyle = this.ctrlLineStyle.value; });
+    this.ctrlLineWidth = new IdRange('.js-linewidth', this.elem).addEventListener('input', () => { this.connector.lineWidth = this.ctrlLineWidth.value; });
     this.ctrlColor = new IdColorPicker('.js-color', this.elem).addEventListener('change', () => { this.connector.color = this.ctrlColor.color; });
     this.btnReverse = this.elem.querySelector('.js-reverse');
     this.btnReverse.addEventListener('click', () => { App.pushUndo(); this.connector.reverse(); });
@@ -55,6 +59,7 @@ export class ConnectorPanel extends Panel implements Subscriber {
         this.ctrlName.value = connector.name; 
         this.ctrlCurve.checked = connector.isCurve;
         this.ctrlOneWay.checked = connector.oneWay;
+        this.ctrlLineWidth.value = connector.lineWidth;
         this.ctrlColor.color = connector.color;
         this.btnReverse.style.display = connector.isDoubleDocked() ? 'block' : 'none';
         this.ctrlStartType.value = connector.startType;
