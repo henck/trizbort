7cf84df13c6ad5b9ae7a55d07148902bbdf88252
Alexander van Oostenrijk <alex.vanoostenrijk@gmail.com>
2020-05-08 15:06:25 +0200

    Add  TS comments

--- src/controls/control.ts
+++ src/controls/control.ts
@@ -1,16 +1,27 @@
-//
-// This is a base class for custom HTML elements. To instantiate,
-// you pass in either an HTML element, or a selector string. 
-// With a selector string, you can also provide a base element
-// that querySelector will run on.
-
 import { Dispatcher } from "../dispatcher";
 import { AppEvent } from "../enums";
 
-// 
+/**
+ * This is a base class for custom HTML elements. To instantiate,
+ * you pass in either an HTML element, or a selector string. 
+ * With a selector string, you can also provide a base element
+ * that querySelector will run on.
+ */
 export class Control {
   protected elem: HTMLElement;
 
+
+  /**
+   * Creates a new Control, using an HtmlElement as a base. Throws an error
+   * if `elem` cannot be found in the DOM.
+   * 
+   * @param elem HtmlElement to use as a base for the Control. If this is a string, 
+   * `querySelector` will be used, searching through the entire document. If a `base`
+   * element is also provided, `querySelector` will run only on elements in base's 
+   * subtree.
+   * @param base Base element that `querySelector` will run on to locate the control's 
+   * element.
+   */
   constructor(elem: HTMLElement|string, base?: HTMLElement) {
     if(elem instanceof HTMLElement) {
       this.elem = elem;
@@ -26,15 +37,25 @@ export class Control {
     }  
   }
 
-  public show = () => {
+  /**
+   * Make the control visible.
+   */
+  public show = (): void => {
     this.elem.style.display = 'block';
   }
 
-  public hide = () => {
+  /**
+   * Make the control invisible.
+   */
+  public hide = (): void => {
     this.elem.style.display = 'none';
   }
 
-  public setVisible = (visible:boolean) => {
+  /**
+   * Set the control's visibility.
+   * @param visible If `true`, Control is made visible. If `false` it's hidden.
+   */
+  public setVisible = (visible:boolean): void => {
     if(visible) {
       this.show();
     } else {
@@ -42,10 +63,13 @@ export class Control {
     }
   }
 
-  //
-  // Add an event listener to the element.
-  // Returns reference to self for easy chaining.
-  // 
+  /**
+   * Add an event listener to the control's element.
+   * @param type Listener type, e.g. 'click'
+   * @param f method to call when event fires
+   * @param refresh If `true` (default), an `AppEvent.Redraw` event is issued to the dispatcher after the event fires.
+   * @returns Reference to self for easy chaining.
+   */ 
   public addEventListener(type: string, f: any, refresh = true): Control {
     let ff: any = (refresh? (e: any) => { f(e); Dispatcher.notify(AppEvent.Redraw, null)}: f );
 
