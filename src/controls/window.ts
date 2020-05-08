c5c4127ad0d625bf958c07336b7ebc5c4a07bb0d
Alexander van Oostenrijk <alex.vanoostenrijk@gmail.com>
2020-05-08 15:11:40 +0200

    Add TS comments

--- src/controls/window.ts
+++ src/controls/window.ts
@@ -1,42 +1,52 @@
-//
-// Window class.
-//
-// Open a window by creating an instance of the Window class:
-//
-//     new Window('Error', 'An error occurred.', true, false);
-//
-// If the onOK argument is true, there will be an OK button. If the 
-// onCancel argument is true, then there will be a cancel button.
-// Any button closes the window when clicked.
-//
-// onOK and onCancel can also be functions. In this case, the function
-// is called when the button is clicked (and the window also closes):
-//
-//     new Window('Delete room?', 'Are you sure...', () => { ... }, false);
-//
-
+/**
+ * Window class.
+ *
+ * Open a window by creating an instance of the Window class:
+ *
+ *     new Window('Error', 'An error occurred.', true, false);
+ *
+ * If the onOK argument is true, there will be an OK button. If the 
+ * onCancel argument is true, then there will be a cancel button.
+ * Any button closes the window when clicked.
+ *
+ * onOK and onCancel can also be functions. In this case, the function
+ * is called when the button is clicked (and the window also closes):
+ *
+ *    new Window('Delete room?', 'Are you sure...', () => { ... }, false);
+ * 
+ */
 export class Window {
   private elem: HTMLElement;
   private onOK: (() => void) | boolean;
   private onCancel: (() => void) | boolean;
 
+  /**
+   * Opens a new Window with optional OK and Cancel buttons.
+   * @param title Window title
+   * @param content Window content
+   * @param onOK If `true`, a OK button is present. If a function, call it when OK button is clicked.
+   * @param onCancel If `true`, a Cancel button is present. If a function, call it when Cancel button is clicked.
+   */
   constructor(title: string, content: string, onOK: (() => void) | boolean, onCancel: (() => void) | boolean) {
     this.elem = document.getElementById('window');
     this.elem.querySelector('.title').innerHTML = title;
     this.elem.querySelector('.content').innerHTML = content;
     this.onOK = onOK;
     this.onCancel = onCancel;
-
     this.open();
   }
 
-  // When OK is pressed, hide the window, and call OK callback if provided:
+  /**
+   *   When OK is pressed, hide the window, and call OK callback if provided:
+   */
   private handleOK = () => {
     this.close();
     if(this.onOK instanceof Function) this.onOK();
   }
 
-  // When Cancel is pressed, hide the window, and call Cancel callback if provided:
+  /**
+   *   When Cancel is pressed, hide the window, and call Cancel callback if provided:
+   */
   private handleCancel = () => {
     this.close();
     if(this.onCancel instanceof Function) this.onCancel();
