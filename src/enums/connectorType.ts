456346415880d95424d261695a3b364f131652d8
Alexander van Oostenrijk <alex.vanoostenrijk@gmail.com>
2020-05-08 09:59:02 +0200

    Add TypeScript comments to ConnectorType and Direction enums so VSCode can show them

--- src/enums/connectorType.ts
+++ src/enums/connectorType.ts
@@ -6,12 +6,20 @@ export enum ConnectorType {
   Down
 }
 
+//
+// Add a static toString method to ConnectorType, so you
+// can write ConnectorType.toString(type)
+// 
 export namespace ConnectorType {
-  export function toString(type: ConnectorType) {
+  /**
+   * Convert a ConnectorType to a string.
+   * @param type ConnectorType to convert
+   */
+  export function toString(type: ConnectorType): string {
     switch(type) {
-      case ConnectorType.In: return 'in';
-      case ConnectorType.Out: return 'out';
-      case ConnectorType.Up: return 'up';
+      case ConnectorType.In:   return 'in';
+      case ConnectorType.Out:  return 'out';
+      case ConnectorType.Up:   return 'up';
       case ConnectorType.Down: return 'dn';
       default:
         return '';
